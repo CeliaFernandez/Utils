2fa97793270d8bc401dce593898cd9f8a95a1a80
CeliaFernandez <celia.fernandez.madrazo@cern.ch>
2021-04-09 09:55:48 +0200

    Add submit option

--- Crab-utils/scrab.py
+++ Crab-utils/scrab.py
@@ -3,17 +3,31 @@ import argparse
 
 parser = argparse.ArgumentParser("scrab version 1.0")
 parser.add_argument('command', help='Command to launch into dir')
-parser.add_argument('-d', dest='directory', type=str)
+parser.add_argument('-d', dest='directory', type=str, default='')
+parser.add_argument('-p', dest='prefix', type=str, default='')
 args = parser.parse_args()
 
-print(args.directory)
 
-for it in os.scandir(args.directory):
-    if it.is_dir():
-        os.system('crab ' + args.command + ' ' + it.path)
+### Crab status
+if 'status' in args.command:
+
+    for it in os.scandir(args.directory):
+        if it.is_dir():
+            os.system('crab ' + args.command + ' ' + it.path)
 
 
+### Crab submit
+if 'submit' in args.command:
 
+    for it in os.scandir(args.directory):
 
+        path = it.path
+        filename = path.split('/')[-1]
+        nprefix = len(args.prefix)
+        file_prefix = filename[:nprefix]
+
+        if args.prefix==file_prefix and filename[-3:]=='.py':
+            os.system('crab ' + args.command + ' ' + it.path)
+       
 
 
